6416fb41f223464ddee0416f0f1fa40a638b2c9c
inma <inma@traffgen.com>
2016-12-16 20:56:06 +0100

    create board and cells

--- game-client.js
+++ game-client.js
@@ -1,14 +1,18 @@
 "use strict";
 
-(function(){
+(function(config){
 
   var Game = function(){
 
     this.gameDetails = {
-      width : 600,
-      height : 600,
-      rows : 3,
-      columns: 3,
+      width : 900,
+      height : 500,
+      boardWidth: 350,
+      boardHeight: 350,
+      ceilWidth: 30,
+      ceilHeight: 30,
+      rows : 6,
+      columns: 6,
       alphabet : 'abcdefghijklmnopqrstuvwxyz'.split('')
     };
     this.canvas = {};
@@ -27,14 +31,17 @@
 
     }
 
+    this.board = {};
+
 
 
     this.ceil = function(){
       return {
-        width : 0,
-        height : 0,
         row : "",
-        column: 0
+        column: 0,
+        data: {},
+        x: 0,
+        y: 0
       }
     };
     this.ColumnsAndRows = [
@@ -47,25 +54,43 @@
   Game.prototype.init = function(){
     console.log("init");
     this.canvas = document.getElementById( 'canvas' );
+    this.canvas.width = this.gameDetails.width;
+    this.canvas.height = this.gameDetails.height;
     this.ctx = canvas.getContext( '2d' );
-    this._createBoard();
-  }
+    this.board = this._createBoard(100, 100 );
+    var board = this._createBoard(300, 100 );
+  };
 
-  Game.prototype._createBoard = function(){
+  Game.prototype._generate = function(){
+    this.gameDetails.ceilWidth = this.gameDetails.boardWidth / this.gameDetails.columns;
+    this.gameDetails.ceilHeight = this.gameDetails.boardHeight / this.gameDetails.rows;
+  };
+
+  Game.prototype._createBoard = function(x, y){
     console.log("create board");
     for(var row = 0; row < this.gameDetails.rows; row++){
       for(var column =0; column < this.gameDetails.columns; column++ ){
         var ceil = new this.ceil();
         ceil.row = this.gameDetails.alphabet[row];
         ceil.column = column;
+        ceil.x = column * this.gameDetails.ceilWidth;
+        ceil.y = row * this.gameDetails.ceilHeight;
         //console.log("new ceil ", ceil, row, column);
         if(!this.ColumnsAndRows[row]){
           this.ColumnsAndRows.push([]);
         }
         this.ColumnsAndRows[row].push(ceil);
+        this._drawACeil(x + ceil.x , y + ceil.y, this.gameDetails.ceilWidth, this.gameDetails.ceilHeight, null, '1');
       }
     }
     console.log("columns and rows ", this.ColumnsAndRows);
+  };
+
+  Game.prototype._drawACeil = function(x, y, width, height, color, lineWidth){
+    this.ctx.lineWidth = lineWidth || '1';
+    this.ctx.beginPath();
+    this.ctx.rect(x , y, width, height);
+    this.ctx.stroke();
   }
 
 
@@ -77,4 +102,4 @@
   		init: battleShip.init.bind(battleShip)
   }
 
-})();
+})(config);
